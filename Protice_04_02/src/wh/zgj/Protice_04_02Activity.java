3f45bb9c01024a71fe695054229b994e05d64175
unknown <380892880@qq.com>
2012-11-07 21:57:13 +0800

    use "TextView" controls in code

---
+++ Protice_04_02/src/wh/zgj/Protice_04_02Activity.java
@@ -0,0 +1,31 @@
+package wh.zgj;
+
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Protice_04_02Activity extends Activity {
+    /** Called when the activity is first created. */
+
+	private TextView textview;
+	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        textview =(TextView)this.findViewById(R.id.m_textview);//
+        textview.setTextColor(Color.RED);
+        textview.setTextSize(20);
+        textview.setBackgroundColor(Color.BLUE);
+        String str = "TextView Ê¾Àý£¬»¶Ó­Ê¹ÓÃ";
+        textview.setText(str);
+        
+    }
+    
+    
+
+}
