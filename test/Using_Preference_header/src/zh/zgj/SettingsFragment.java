9744e70d843e54543442d8c1f54c706c7ddb81a7
unknown <380892880@qq.com>
2013-01-27 23:48:46 +0800

    learn settings header xml

--- test/Using_Preference_header/src/zh/zgj/SettingsFragment.java
+++ test/Using_Preference_header/src/zh/zgj/SettingsFragment.java
@@ -7,9 +7,15 @@ public class SettingsFragment extends PreferenceFragment{
 
 	 @Override
 	 public void onCreate(Bundle savedInstanceState) {
-	  // TODO Auto-generated method stub
-	  super.onCreate(savedInstanceState);
-	  addPreferencesFromResource(R.xml.preferences);
+		  // TODO Auto-generated method stub
+		  super.onCreate(savedInstanceState);
+		  String flag = getArguments().getString("someKey");
+		  if("wh".equals(flag)){
+			  addPreferencesFromResource(R.xml.test_preferences);
+		  }else if("zgj".equals(flag)){
+			  addPreferencesFromResource(R.xml.preferences);
+		  }
+		  
 	 }
 
 }
--- test/Using_Preference_header/src/zh/zgj/SetttingsActivity.java
+++ test/Using_Preference_header/src/zh/zgj/SetttingsActivity.java
@@ -2,14 +2,23 @@ package zh.zgj;
 
 import java.util.List;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 public class SetttingsActivity extends PreferenceActivity {
-
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "zgj", 3000).show();
+    }
 	 @Override
 	 public void onBuildHeaders(List<Header> target) {
-	  // TODO Auto-generated method stub
-	  super.onBuildHeaders(target);
-	  loadHeadersFromResource(R.xml.preferenceheader,target);
+		 
+		  // TODO Auto-generated method stub
+		  super.onBuildHeaders(target);
+		  
+		  loadHeadersFromResource(R.xml.preferenceheader,target);
+		  Toast.makeText(this, "3.0 update", 2000).show();
 	 }
 }
